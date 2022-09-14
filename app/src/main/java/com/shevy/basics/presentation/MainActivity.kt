51c5f68f384b9fab24bf4a40bd2b133a2e282821
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-09-14 21:48:36 +0500

    Added Koin

--- app/src/main/java/com/shevy/basics/presentation/MainActivity.kt
+++ app/src/main/java/com/shevy/basics/presentation/MainActivity.kt
@@ -6,16 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.shevy.basics.databinding.ActivityMainBinding
-import com.shevy.data.repository.UserRepositoryImpl
-import com.shevy.data.storage.sharedprefs.SharedPrefUserStorage
-import com.shevy.domain.models.SaveUserNameParam
-import com.shevy.domain.models.UserName
-import com.shevy.domain.usecase.GetUserNameUseCase
-import com.shevy.domain.usecase.SaveUserNameUseCase
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    private lateinit var vm: MainViewModel
+    private val vm by viewModel<MainViewModel>()
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +18,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         Log.d("AAA", "Activity created")
-
-        vm = ViewModelProvider(this, MainViewModelFactory(this))[MainViewModel::class.java]
 
         val dataTextView = binding.dataTextView
         val dataEditView = binding.dataEditText
